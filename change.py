42ecb8b550ad741201c7c9af6571921a89ae2da8
sammy4321 <samarth.m.gowda@gmail.com>
2017-05-07 00:35:29 -0400

    Commit Message

---
+++ change.py
@@ -0,0 +1,12 @@
+import cv2
+img=cv2.imread("cropped.jpg",0)
+#gray=cv2.cvtColor(img.cv2.COLOR_BGR2GRAY)
+for i in range(0,28):
+	for j in range(0,28):
+		if(img[i][j]==255):
+			img[i][j]=0
+		else:
+			img[i][j]=255
+cv2.imwrite("cropped.jpg",img)
+cv2.imshow("cropped",img)
+cv2.waitKey(0)
